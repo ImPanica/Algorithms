c3829275850a4b9269ccc58b573c4e05df561773
yarshevich1 <euheniilins@gmail.com>
2024-06-09 15:20:39 +0300

    Added binary search

---
+++ Algorithms/Algoritms/Binary/Binary.cs
@@ -0,0 +1,55 @@
+﻿namespace Algorithms.Binary
+{
+    internal class Binary
+    {
+        public static int BinarySearchToInt(List<int> list, int value)
+        {
+            // Переменные для хранения границ
+            int low = 0;
+            int high = list.Count - 1;
+
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2; // Делим список на 2
+                int tempValue = list[mid]; // заносим во временную переменную
+
+                if (tempValue == value) // Если равно, то возвращаем
+                    return mid;
+
+                if (tempValue > value) // Много
+                    high = mid - 1;
+                else                   // Мало
+                    low = mid + 1;
+            }
+
+            return -1; // значение отсутствует
+        }
+
+        public static int BinarySearchToString(List<string> list, string value)
+        {
+            // Переменные для хранения границ
+            int low = 0;
+            int high = list.Count - 1;
+
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2; // Делим список на 2
+                string tempValue = list[mid]; // заносим во временную переменную
+                int compareResult = tempValue.CompareTo(value); // Получаем результат сравнения слов в интовом числе
+                                                                // -1 число меньше
+                                                                // 0 равно
+                                                                // 1 число больше
+
+                if (compareResult == 0) // Если равно, то возвращаем индекс
+                    return mid;
+
+                if (compareResult > 0) // Если значение больше, то ищем в левой стороне
+                    high = mid - 1;
+                else
+                    low = mid + 1; // Если значение меньше, то ищем в правой стороне
+
+            }
+            return -1; // Значение не найдено
+        }
+    }
+}
---
+++ Algorithms/Algoritms/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Бинарный поиск
+            List<int> numbers = Enumerable.Range(1, 100).ToList();
+            numbers.Sort();
+
+            Console.WriteLine($"{Binary.Binary.BinarySearchToInt(numbers, 5)}");
+            Console.WriteLine($"{Binary.Binary.BinarySearchToInt(numbers, 500)}");
+            Console.WriteLine($"{Binary.Binary.BinarySearchToInt(numbers, 77)}");
+            Console.WriteLine($"{Binary.Binary.BinarySearchToInt(numbers, 99)}");
+
+            List<string> sortedList = new List<string> { "apple", "banana", "cherry", "date", "fig", "grape" };
+            Console.WriteLine($"Position found word: {Binary.Binary.BinarySearchToString(sortedList, "fig")}");
+            Console.WriteLine($"Position found word: {Binary.Binary.BinarySearchToString(sortedList, "grape")}");
+            Console.WriteLine($"Position found word: {Binary.Binary.BinarySearchToString(sortedList, "banana")}");
+        }
+    }
+}
